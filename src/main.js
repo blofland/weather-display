3b5c708b0a9e0b9b595d3ce69dcb54ec144f3693
Nate <boedi15@gmail.com>
2021-11-27 14:47:58 -0700

    main api function

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ const baseUrl = "https://api.openweathermap.org/data/2.5"
 const getWeather = (city) => {
     // Make API Calls
     // Return an object that holds information about current and forecast
+    const firstUrl = baseUrl + "/weather?q=" + city + "&appid=" + appId 
+    let prom = fetch(firstUrl).then(res => res.json())
+    prom.then(weatherData => {
+        const secondUrl = `${baseUrl}/onecall?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=${appId}`
+        return fetch(secondUrl).then(res => res.json()).then(data => console.log(data))
+    })
+    return prom 
 }
+
+getWeather("Salt Lake City")
