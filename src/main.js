b0acd4e848b84191ac699ed187f729239db2daa0
Nate <boedi15@gmail.com>
2021-11-27 15:04:32 -0700

    Refactored api calls out of getWeather and made it async

--- src/main.js
+++ src/main.js
@@ -2,16 +2,17 @@
 const appId = "ba2aac9b1d7724e46122e0527e8241b3"
 const baseUrl = "https://api.openweathermap.org/data/2.5"
 
-const getWeather = (city) => {
+const getWeather = async (city) => {
     // Make API Calls
     // Return an object that holds information about current and forecast
-    const firstUrl = baseUrl + "/weather?q=" + city + "&appid=" + appId 
-    let prom = fetch(firstUrl).then(res => res.json())
-    prom.then(weatherData => {
-        const secondUrl = `${baseUrl}/onecall?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=${appId}`
-        return fetch(secondUrl).then(res => res.json()).then(data => console.log(data))
-    })
-    return prom 
+    try {
+        let weather = await getCoordForCity(city)
+        weather = {...weather, ...await getDataFromCoord(weather.coord)}
+        return weather 
+    } catch(err){
+        console.error(err)
+        alert("Looks like that was a bad request, check your spelling and try again")
+    }
 }
 
 const getCoordForCity = async city =>{
