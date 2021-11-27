170b14db5c0663bc281e3e7b70489c2482315e6c
Nate <boedi15@gmail.com>
2021-11-27 15:12:49 -0700

    get Store item from local storage

--- src/storage.js
+++ src/storage.js
@@ -1,4 +1,7 @@
 const useStorage = (name="data", defaultObj={}) => {
     name = name.toLocaleLowerCase()
     if(!localStorage.getItem(name)) localStorage.setItem(name, JSON.stringify(defaultObj))
+
+    const getStore = () => JSON.parse(localStorage.getItem(name)) 
+
 }
