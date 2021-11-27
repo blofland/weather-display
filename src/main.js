ceb529681de1ad152af1d613d2fe182a59e3fa94
Nate <boedi15@gmail.com>
2021-11-27 15:07:10 -0700

    Parse out relevant data from weatherData

--- src/main.js
+++ src/main.js
@@ -6,9 +6,34 @@ const getWeather = async (city) => {
     // Make API Calls
     // Return an object that holds information about current and forecast
     try {
-        let weather = await getCoordForCity(city)
-        weather = {...weather, ...await getDataFromCoord(weather.coord)}
-        return weather 
+        let weatherData = await getCoordForCity(city)
+        weatherData = {...weatherData, ...await getDataFromCoord(weatherData.coord)}
+        const current = {
+            city: weatherData.name, 
+            dt: weatherData.dt * 1000,
+            humidity: weatherData.current.humidity, 
+            uvi: weatherData.current.uvi,
+            wind: weatherData.current.wind_speed,
+            icon: `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`,
+            temp: weatherData.current.temp,
+            lat: weatherData.lat, 
+            lon: weatherData.lon
+        }
+    
+        const forecast= weatherData.daily.slice(0,5).map(day => {
+            return {
+                city: weatherData.name, 
+                dt: day.dt * 1000, 
+                humidity: day.humidity, 
+                uvi: day.uvi,
+                wind: day.wind_speed,
+                icon: `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`,
+                temp: day.temp.day,
+                lat: weatherData.lat, 
+                lon: weatherData.lon
+            }
+        })
+        return {current, forecast}
     } catch(err){
         console.error(err)
         alert("Looks like that was a bad request, check your spelling and try again")
@@ -24,5 +49,3 @@ const getDataFromCoord = async coord => {
     const url = `${baseUrl}/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${appId}&units=imperial`
     return await fetch(url).then(res=> res.json())
 }
-
-getWeather("Salt Lake City")
